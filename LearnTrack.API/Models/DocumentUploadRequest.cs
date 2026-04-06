745ed056066a755940400f96c422938fbc153b05
RAVISHANKAR YADAV <146420561+ravi-8545@users.noreply.github.com>
2026-04-06 10:09:46 +0530

    feat: add document API and implement password hashing

---
+++ LearnTrack.API/Models/DocumentUploadRequest.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Http;
+
+public class DocumentUploadRequest
+{
+    public IFormFile File { get; set; } = null!;
+    public Guid EmployeeId { get; set; }
+    public Guid AssignmentId { get; set; }
+}
